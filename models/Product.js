b3d8ad737b4d2a12d9d978bee366061c500c9bae
edodgion <barry.manowar@gmail.com>
2021-05-14 18:10:32 -0600

    2

--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false
   },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+    //* Validates that the value is a decimal.
+  },
+
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 10,
+    //* Validates that the value is numeric.
+  },
+
+  category_i: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'traveller',
+      key: 'id',
+      unique: false
+    }
+  }
+},
   {
     sequelize,
     timestamps: false,
--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsToMany(Category, {
 
 // Categories have many Products
+through: {
+  model: Trip,
+  unique: false
+},
+as: 'planned_trips'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'planned_trips'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'planned_trips'
+});
 
 module.exports = {
   Product,
